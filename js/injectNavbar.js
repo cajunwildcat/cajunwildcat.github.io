593955fc596c9f922375c86a704790d1d60d10fd
Eli Cheramie <elicheramie89@gmail.com>
2024-04-15 20:11:35 -0400

    added portfolio as main page

---
+++ js/injectNavbar.js
@@ -0,0 +1,10 @@
+document.write(`
+    <link rel="stylesheet" href="css/navbarStyle.css">
+    <div id="navbar">
+        <a id="homeButton" class="navItem" href="index.html"><p>Home</p></a>
+        <a class="navItem" href="index.html#about"><p>About</p></a>
+        <a class="navItem" href="index.html#projects"><p>Projects</p></a>
+        <a class="navItem" href="index.html#contact"><p>Contact</p></a>
+        <a class="navItem" href="media/Eli-Cheramie-Resume.pdf" target="_blank"><p>Resume</p></a>
+    </div>
+`);
